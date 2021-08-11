651956d36c88f6149101857e8776e3671fe8671f
Søren Fuglede Jørgensen <github@fuglede.dk>
2021-08-11 23:16:57 +0200

    Add a test illustrating suboptimality

--- NumberPartitioning.Tests/KarmarkarKarpTest.cs
+++ NumberPartitioning.Tests/KarmarkarKarpTest.cs
@@ -9,22 +9,32 @@ namespace NumberPartitioning.Tests
         [Fact]
         public void TestHeuristicWorks()
         {
-            var numbers = new [] {4, 6, 7, 5, 8};
+            var numbers = new[] { 4, 6, 7, 5, 8 };
             var (partition, sizes) = KarmarkarKarp.Heuristic(numbers, 3);
-            Assert.Equal(new [] { 8, 11, 11 }, sizes);
+            Assert.Equal(new[] { 8, 11, 11 }, sizes);
             Assert.Equal(
-                new List<int>[] {new() {8}, new() {4, 7}, new() {5, 6}},
+                new List<int>[] { new() { 8 }, new() { 4, 7 }, new() { 5, 6 } },
                 partition);
         }
 
         [Fact]
         public void TestHeuristicCanGiveIndices()
         {
-            var numbers = new [] { 4, 6, 7, 5, 8 };
+            var numbers = new[] { 4, 6, 7, 5, 8 };
             var (partition, sizes) = KarmarkarKarp.Heuristic(numbers, 3, true);
             Assert.Equal(new[] { 8, 11, 11 }, sizes);
             Assert.Equal(
                 new List<int>[] { new() { 4 }, new() { 0, 2 }, new() { 3, 1 } },
+                partition);
+        }
+        [Fact]
+        public void TestHeuristicSuboptimal()
+        {
+            var numbers = new[] { 5, 5, 5, 4, 4, 3, 3, 1 };
+            var (partition, sizes) = KarmarkarKarp.Heuristic(numbers, 3);
+            Assert.Equal(new[] { 9, 10, 11 }, sizes);
+            Assert.Equal(
+                new List<int>[] { new() { 4, 5 }, new() { 1, 4, 5 }, new() { 3, 3, 5 } },
                 partition);
         }
 
